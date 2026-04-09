19747c77f61803c1b80a52a3b1d52d284ad71e14
MBANS8A1 <seanmbng@aol.com>
2026-04-09 18:36:10 +0100

    I created a posts list in the blog app views.py file. It is still a dummy list consisting of dictionaries as its elements.  This starts the process of moving away from hardcoded templates for my project.

--- blogs_site/blog/views.py
+++ blogs_site/blog/views.py
@@ -1,4 +1,29 @@
+from datetime import date
 from django.shortcuts import render
+
+posts = [
+    {
+        "slug": "biking-in-the-mountains",
+        "image" : "mountain_biking_sm_jpg",
+        "author": "Sean Crossahm",
+        "date": date(2026,1,14),
+        "title": "Mountain Biking",
+        "excerpt": "The satisfaction you get from seeing the views from your bike on the mountain course is indescribable. I was not prepared for what happened, as soon as I got back to riding further along the path!",
+        "content" : """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Architecto alias vel quasi quo. Aperiam ex labore cumque reiciendis in atque,
+          tenetur porro sint molestias ut officia exercitationem? Sequi, fuga officiis.
+          
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Architecto alias vel quasi quo. Aperiam ex labore cumque reiciendis in atque,
+          tenetur porro sint molestias ut officia exercitationem? Sequi, fuga officiis.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Architecto alias vel quasi quo. Aperiam ex labore cumque reiciendis in atque,
+          tenetur porro sint molestias ut officia exercitationem? Sequi, fuga officiis.
+          """
+    }
+]
 
 # Create your views here.
 

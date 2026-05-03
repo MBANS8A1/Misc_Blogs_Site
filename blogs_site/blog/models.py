c9fcc7b6052a586137409824c4a2606db0e94f93
MBANS8A1 <seanmbng@aol.com>
2026-05-03 19:40:59 +0100

    I added the Tag model to the models.py file, which will help in categorizing different Posts depending upon the caption. I connected the Tag and Post models via a many-to-many relationship with the tags field.

--- blogs_site/blog/models.py
+++ blogs_site/blog/models.py
@@ -3,6 +3,8 @@ from django.core.validators import MinLengthValidator
 # Create your models here.
 
 
+class Tag(models.Model):
+    caption = models.CharField(max_length=10)
 
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
@@ -19,3 +21,4 @@ class Post(models.Model):
     slug = models.SlugField(unique=True,db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(Author,on_delete=models.SET_NULL,related_name="posts")
+    tags = models.ManyToManyField(Tag)

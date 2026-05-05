d7943c00f770144db884864c8c0581f1b0c133f9
MBANS8A1 <seanmbng@aol.com>
2026-05-05 20:46:05 +0100

    In the entry_page view function, I removed line 16 and 17, which fetches posts and produces a new list with the them ordered by data and the three most recent posts being fetched by negative indexing. In its place I add a one-line query which fetches and orders the Posts by date in a descending order with the three most recent posts being selected and stored within the latest_posts variable.

--- blogs_site/blog/views.py
+++ blogs_site/blog/views.py
@@ -1,5 +1,6 @@
 from datetime import date
 from django.shortcuts import render
+from .models import Post
 
 all_posts = [
     
@@ -13,8 +14,7 @@ def get_date(post):
 
 #To display the starting page
 def entry_page(request):
-    sorted_posts = sorted(all_posts,key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request,"blog/index.html",{
         "posts": latest_posts
     })

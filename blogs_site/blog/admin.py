29261282e34a5685a5c3480db615acd3b39143b3
MBANS8A1 <seanmbng@aol.com>
2026-05-05 10:56:59 +0100

    I have customized the django admin panel for the Post data by adding a PostAdmin class with list_filter and list_display properties so the details of a Post object can be view more clearly according to the columns and can be filtered.

--- blogs_site/blog/admin.py
+++ blogs_site/blog/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Post,Tag,Author
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author","tags","date")
+    list_display = ("title","date","author")
+
+admin.site.register(Post,PostAdmin)
 admin.site.register(Tag)
 admin.site.register(Author)

e8c2894c8d4a78e1dfaa28dafe2940aaf70a580c
MBANS8A1 <seanmbng@aol.com>
2026-05-29 22:53:21 +0100

    The tags on the details page of a post were not showing up so I had to override the get_context_data() method in order to add the post_tags field in post-detail.html to the context as a key. By accessing the DetailView object data with self, I retrieved all the tags for a post. I finally returned the updated context dictionary.

--- blogs_site/blog/views.py
+++ blogs_site/blog/views.py
@@ -38,5 +38,10 @@ class SinglePostView(DetailView):
     model = Post
     slug_url_kwarg = "slugPost"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tags.all()
+        return context
+
 
 
